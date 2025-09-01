bc499640eaa71da2167ba1cf90aa6fc8fd748e73
Victor Esquinas Roque <v.esquinasque@gmail.com>
2025-09-01 11:24:23 +0200

    fix(victor) working collatz sequence script
    Working script

--- collatz_sequence.py
+++ collatz_sequence.py
@@ -1,33 +1,30 @@
 players_number = None
-global_number = None
 
 def collatz(number):
     
     if number % 2 == 0:
-        print(f"{number}", end=' ')
         return number // 2
-    global global_number
-    global_number = number
-    else:
-        print(f"{number}", end=' ')
-        return 3 * number + 1
-    global global_number
-    global_number = number
+    return 3 * number + 1
 
 while True:
     while players_number == None:
+
         try:
             print(f"Type a number:")
             players_number = int(input('>'))
+
         except ValueError:
             print (f"Please enter an integer")
             continue
-    collatz(players_number)
     
-    if global_number == 1:
+        result = collatz(players_number)
+
+    if result == 1:
         break
     else:
-        collatz(global_number)
+        result = collatz(result)
+    
+    print(f"{result}", end =' ')
 
 
         
